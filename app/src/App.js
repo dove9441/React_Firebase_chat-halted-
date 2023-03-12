f7ab775293a6490ba7862e21460ebf1a496dd57e
dove9441 <dotml386@naver.com>
2023-03-12 10:17:05 +0000

    react basic 230312

---
+++ app/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+// react router DOM을 이용하기 위해
+import {
+	BrouwserRouter as Router,
+	Switch,
+	Route,
+	Link
+} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
